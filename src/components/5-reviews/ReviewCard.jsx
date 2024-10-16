2a9ef36f93b68be4923f0f7049421da21a5d6eaa
kamel <kamelsamy3@gmail.com>
2024-10-17 02:54:02 +0300

    importing swiper library for slides and add review cards to review component

---
+++ src/components/5-reviews/ReviewCard.jsx
@@ -0,0 +1,29 @@
+export default function ReviewCard({ name, stars }) {
+  const renderStars = (stars) => {
+    const starElements = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < stars) {
+        starElements.push(<span key={i}>⭐</span>);
+      } else {
+        starElements.push(<span key={i}>☆</span>);
+      }
+    }
+    return starElements;
+  };
+
+  return (
+    <div className="review flex">
+      <div className="avatar flex">
+        <div className="icon-user-circle"></div>
+        <h3>{name}</h3>
+      </div>
+      <div className="stars">{renderStars(stars)}</div>
+      <p className="date">may 22.2012</p>
+      <p className="opinion">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus
+        doloribus modi ipsum repudiandae quia sequi non eaque maxime dolore
+        eveniet, quisquam, sapiente tempor
+      </p>
+    </div>
+  );
+}
--- src/components/5-reviews/Reviews.jsx
+++ src/components/5-reviews/Reviews.jsx
@@ -1,5 +1,27 @@
 import "./reviews.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ReviewCard from "./ReviewCard";
+import "swiper/css";
 
 export default function Reviews() {
-  return <p>Reviews</p>;
+  return (
+    <section className="reviews">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <ReviewCard name="Camel" stars={5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard name="Jane Doe" stars={4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewCard name="Jane " stars={4.5} />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
 }
