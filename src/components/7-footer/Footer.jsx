23db5a6ddafca84c509fd4fa762d31c2b5a33cd5
kamel <kamelsamy3@gmail.com>
2024-10-18 15:34:11 +0300

    make the website more responsive and manage light mode and dark mode

--- src/components/7-footer/Footer.jsx
+++ src/components/7-footer/Footer.jsx
@@ -1,15 +1,23 @@
-import './footer.css'
+import "./footer.css";
 
-export default function Footer(){
-    return(
-        <footer className='flex'>
-            <ul className='flex left-side'>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Speaking</a></li>
-                <li><a href="#">Uses</a></li>
-            </ul>
-            <p className='right-side'>&copy; Spencer Sharp.All rights reserved.</p>
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <footer className="flex">
+      <ul className="flex left-side">
+        <li>
+          <span> </span>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#articles">Articles</a>
+        </li>
+      </ul>
+      <p className="right-side">&copy; Spencer Sharp.All rights reserved.</p>
+    </footer>
+  );
 }
